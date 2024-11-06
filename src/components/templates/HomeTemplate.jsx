d9ac6325335d2fea14562407a24a5656797d180a
Cinthya97Almaguer <99464245+Cinthya97Almaguer@users.noreply.github.com>
2024-11-06 08:35:00 -0600

    Añadir Sidebar y SidebardCard, mejorar estilos en HomeTemplate y actualizar estructura de App

---
+++ src/components/templates/HomeTemplate.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+export function HomeTemplate() {
+    return (<Containeir>
+        <h1>Home Template</h1>
+    </Containeir>);
+}
+const Containeir = styled.div` 
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:100vh;
+    overflow:hidden;
+    background-color:${({ theme }) => theme.bgtotal};
+    color:${({ theme }) => theme.text};
+    width:100%;
+`
